5592f79292192e43823cd5279b5725fc5fae3086
Joker-Security <randjiaa@gmail.com>
2018-04-24 08:02:42 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -87,7 +87,7 @@ clear
 ;;
 '2')
 echo -e $green "[*] Loading... "
-sudo pacman -Syu
+sudo pacman -Syy
 sudo pacman -S python2-pip
 echo "[*] installing requirements...."
 pip2 install -r requirements.txt
